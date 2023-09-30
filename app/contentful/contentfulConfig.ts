3f2f35602c249e26ef4d22f02e7bc594f258369d
k1d12r04 <k1d12r04@gmail.com>
2023-09-30 17:24:06 +0300

    Setup contentful config and react query and get data

---
+++ app/contentful/contentfulConfig.ts
@@ -0,0 +1,6 @@
+const contentfulConfig = {
+  spaceId: '513yrt3pwfxx',
+  accessToken: 'UJF0T0kuAfGZ9_tNZpaZ1Am98OU09QA2wHynmaSQ6No',
+};
+
+export default contentfulConfig;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,44 @@
-export default function Home() {
-  return <div>hello</div>;
-}
+'use client';
+
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { AxiosError } from 'axios';
+import contentfulConfig from './contentful/contentfulConfig';
+
+type fields = {
+  id: number;
+  question: string;
+  answers: string[];
+  rightAnswer: string;
+};
+
+type QuizData = {
+  fields: fields;
+};
+
+const HomePage = () => {
+  const { data, isLoading, isError } = useQuery<QuizData[]>(
+    'contentfulData',
+    fetchQuizData
+  );
+
+  async function fetchQuizData() {
+    try {
+      const { spaceId, accessToken } = contentfulConfig;
+      const response = await axios.get(
+        `https://cdn.contentful.com/spaces/${spaceId}/entries?access_token=${accessToken}`
+      );
+
+      return response.data.items;
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      throw new Error(
+        'Error fetching data from Contentful: ' + axiosError.message
+      );
+    }
+  }
+
+  console.log(data);
+};
+
+export default HomePage;
