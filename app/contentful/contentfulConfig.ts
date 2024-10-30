1fa51b1e98da39f33de5dea5242b643f6d0a8a43
k1d12r04 <k1d12r04@gmail.com>
2024-10-30 22:29:55 +0300

    Correct token ids

--- app/contentful/contentfulConfig.ts
+++ app/contentful/contentfulConfig.ts
@@ -1,6 +1,6 @@
 export const contentfulConfig = {
-  spaceId: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  spaceId: process.env.NEXT_PUBLIC_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
 };
 
 export default contentfulConfig;
